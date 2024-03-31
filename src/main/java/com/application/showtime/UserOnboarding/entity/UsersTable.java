fd15ba2f6700cb567aeb32c78d5eefc638f8e3c3
Prajwal0099 <96511834+Prajwal0099@users.noreply.github.com>
2024-03-31 20:32:21 +0530

    Added folder structure for userOnboarding and updated userTable (#3)
    * Added folder structure for userOnboarding and updated userTable
    
    * Added 'Spring starter Validation Dependency' and Updated application.properties file.

---
+++ src/main/java/com/application/showtime/UserOnboarding/entity/UsersTable.java
@@ -0,0 +1,38 @@
+package com.application.showtime.UserOnboarding.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "SHOWTIME_USERS")
+public class UsersTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userId;
+
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "First name must contain only alphabets")
+    @Size(min = 2, message = "First name must be at least 2 characters long")
+    private String firstName;
+
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Last name must contain only alphabets")
+    @Size(min = 2, message = "Last name must be at least 2 characters long")
+    private String lastName;
+
+    @Digits(integer = 10, fraction = 0, message = "Mobile number must be a 10-digit integer")
+    private long mobileNumber;
+
+    private String address;
+
+    @Email
+    private String emailId;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "Incorrect Password format")
+    private String password;
+
+}

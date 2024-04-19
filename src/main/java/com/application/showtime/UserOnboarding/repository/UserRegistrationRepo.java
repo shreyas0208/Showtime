438eb45435a4000cf3c8c4b3c6c9673fb136efb4
Prajwal0099 <96511834+Prajwal0099@users.noreply.github.com>
2024-04-19 22:12:09 +0530

    Implemented GET, DELETE and  POST APIs for getting list of all registered user, deleting user with particular ID and login of user respectively. (#7)
    Implemented GET, DELETE APIs for getting list of all registered users and deleting user with particular ID repectively and Implemented POST API for login of user

--- src/main/java/com/application/showtime/UserOnboarding/repository/UserRegistrationRepo.java
+++ src/main/java/com/application/showtime/UserOnboarding/repository/UserRegistrationRepo.java
@@ -1,10 +1,14 @@
 package com.application.showtime.UserOnboarding.repository;
 
 import com.application.showtime.UserOnboarding.entity.UserRegistrationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRegistrationRepo extends CrudRepository<UserRegistrationEntity,Long> {
+import java.util.Optional;
 
+@Repository
+public interface UserRegistrationRepo extends JpaRepository<UserRegistrationEntity,Long> {
+
+    Optional<UserRegistrationEntity> findUserRegistrationEntityByEmailId(String mail);
 }
--- src/main/java/com/application/showtime/UserOnboarding/serviceImplementation/UserRegistrationServiceImpl.java
+++ src/main/java/com/application/showtime/UserOnboarding/serviceImplementation/UserRegistrationServiceImpl.java
@@ -5,7 +5,11 @@ import com.application.showtime.UserOnboarding.repository.UserRegistrationRepo;
 import com.application.showtime.UserOnboarding.service.UserRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.application.showtime.UserOnboarding.service.UserRegistrationService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserRegistrationServiceImpl implements UserRegistrationService {
@@ -15,5 +19,20 @@ public class UserRegistrationServiceImpl implements UserRegistrationService {
     @Override
     public void createNewUser(UserRegistrationEntity userRegistrationEntity) {
         userRegistrationRepo.save(userRegistrationEntity);
+    }
+
+    @Override
+    public List<UserRegistrationEntity> getAllUsers() {
+        return userRegistrationRepo.findAll();
+    }
+
+    @Override
+    public void deleteUser(long id) {
+        userRegistrationRepo.deleteById(id);
+    }
+
+    @Override
+    public Optional<UserRegistrationEntity> getUserByMail(String mail) {
+        return userRegistrationRepo.findUserRegistrationEntityByEmailId(mail);
     }
 }
